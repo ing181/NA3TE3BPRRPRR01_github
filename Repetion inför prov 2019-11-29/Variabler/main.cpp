4f7f2779ec0c93627ac7c1e2ad25f0b20c44f197
Ingemar Ceicer <ingcei0123@lerum.local>
2019-11-12 12:30:22 +0100

    Om variabler

---
+++ Repetion inför prov 2019-11-29/Variabler/main.cpp
@@ -0,0 +1,36 @@
+// Variabler
+#include <iostream>
+using namespace std;
+int main() {
+/*
+Variabler måste deklareras (måste finnas)
+innan de kan användas
+Variabler deklareras alltid så här
+typ namn;
+typ avser vad för slags data som variabeln
+kan innehålla.
+EX:
+int k; kan innehålla heltal
+Namngining: Ej börja med en siffra,
+ej innehålla mellanslag
+Undvik svenska tecken (åäö...)
+
+Förutom typ av data
+int (heltal), float för flyttal
+kan variabler innehålla olika MYCKET data
+doble och float kan båda innehålla flyttat
+men double kan innehålla ett större tal
+*/
+int i;  // Deklaration
+i = 6;  // Används
+// Alternativt
+int j = 5;
+// Se alltid till att du tilldelat 
+// värde innan du använder en variabels värde
+// float a; a har ett odifinierat värde
+float a=0; 
+a = (a+1);
+
+	return 0;
+}
+
