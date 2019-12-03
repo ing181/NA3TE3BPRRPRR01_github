697f7840c8ce7acabce0ff7c2ad4f16c51de32d9
Ingemar Ceicer <ingcei0123@lerum.local>
2019-12-03 14:30:10 +0100

    kopieras

--- 2019-12-03/funktion/main.cpp
+++ 2019-12-03/funktion/main.cpp
@@ -10,6 +10,7 @@ int main() {
 	
 	//float utvarde = 8.5;
 	float utvarde = addera(2.4, 6.1); // Här används funktionen (Anropas)
+									  // 2.4 kopieras till x och 6.1 kopieras till y
     cout << utvarde << endl;
 	return 0;
 }
