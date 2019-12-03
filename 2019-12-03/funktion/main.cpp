9e64f7abe674f9b5010db75b4de5e37b0424bf92
Ingemar Ceicer <ingcei0123@edu.lerum.se>
2019-12-03 14:30:58 +0100

    Update main.cpp

--- 2019-12-03/funktion/main.cpp
+++ 2019-12-03/funktion/main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
-// Funktioner deklareras sÂ h‰r
-// datatyp_utv‰rde namn(datatyp_inv‰rde, datatyp_inv‰rde, ...);
-// Deklarerar utv‰rde, namn och inv‰rde (inv‰rden)
+// Funktioner deklareras s√• h√§r
+// datatyp_utv√§rde namn(datatyp_inv√§rde, datatyp_inv√§rde, ...);
+// Deklarerar utv√§rde, namn och inv√§rde (inv√§rden)
 
-// Funktion som adderar tvÂ tal
+// Funktion som adderar tv√• tal
 float addera(float, float); // Funktionsdeklaration
 int main() {
 	
 	//float utvarde = 8.5;
-	float utvarde = addera(2.4, 6.1); // H‰r anv‰nds funktionen (Anropas)
-									  // 2.4 kopieras till x och 6.1 kopieras till y
+	float utvarde = addera(2.4, 6.1); // H√§r anv√§nds funktionen (Anropas)
+				          // 2.4 kopieras till x och 6.1 kopieras till y
     cout << utvarde << endl;
 	return 0;
 }
-// H‰r slutar huvudprogrammet
-float addera(float x, float y) // Funktionsdefinition "Vad den gˆr"
+// H√§r slutar huvudprogrammet
+float addera(float x, float y) // Funktionsdefinition "Vad den g√∂r"
 {
 	return (x+y); // Skickas tillbaka
 }
